60b99781125fd43baca74258ef9e90847d4524e5
Kurtis Jones <battousai999@gmail.com>
2020-08-28 16:38:12 -0600

    Add PropertyEqualityComparer class
    This class allows one to provide instances of IEqualityComparer<T> using just a
    projection on the type T (and potentially another IEqualityComparer<U> where type
    U is the return type of the projection).  This allows class instances to be
    compared based on a property of the class or some calculation on the instance of
    the class.

---
+++ ConsoleUtils.Tests/PropertyEqualityComparerTests.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+using Battousai.Utils;
+
+namespace ConsoleUtils.Tests
+{
+    public class PropertyEqualityComparerTests
+    {
+        public class TestClass
+        {
+            public int IntValue { get; set; }
+            public string StrValue { get; set; }
+        }
+
+        public class ConstructorTests
+        {
+            [Fact]
+            public void ThrowsException_IfConstructorCalledWithoutProjection()
+            {
+                Should.Throw<ArgumentNullException>(() => { new PropertyEqualityComparer<TestClass, int>(null); });
+            }
+
+            [Fact]
+            public void DoesNotThrowException_IfConstructorCalledWithoutComparer()
+            {
+                Should.NotThrow(() => { new PropertyEqualityComparer<TestClass, string>(x => x.StrValue, comparer: null); });
+            }
+        }
+
+        public class EqualsTests
+        {
+            [Fact]
+            public void ReturnsTrue_IfBothParametersAreNull()
+            {
+                var subject = new PropertyEqualityComparer<TestClass, int>(x => x.IntValue);
+
+                var results = subject.Equals(null, null);
+
+                results.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void ReturnsFalse_IfEitherButNotBothParametersAreNull()
+            {
+                var test = new TestClass();
+                var subject = new PropertyEqualityComparer<TestClass, int>(x => x.IntValue);
+
+                var results1 = subject.Equals(null, test);
+                var results2 = subject.Equals(test, null);
+
+                results1.ShouldBeFalse();
+                results2.ShouldBeFalse();
+            }
+
+            [Fact]
+            public void ReturnsTrue_IfProjectionsOfBothParametersAreNull()
+            {
+                var test1 = new TestClass { StrValue = null };
+                var test2 = new TestClass { StrValue = null };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue);
+
+                var results = subject.Equals(test1, test2);
+
+                results.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void ReturnsFalse_IfProjectionsOfEitherButNotBothParametersAreNull()
+            {
+                var test1 = new TestClass { StrValue = "testing" };
+                var test2 = new TestClass { StrValue = null };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue);
+
+                var results1 = subject.Equals(test1, test2);
+                var results2 = subject.Equals(test2, test1);
+
+                results1.ShouldBeFalse();
+                results2.ShouldBeFalse();
+            }
+
+            [Fact]
+            public void ReturnsTrue_IfProjectionsOfBothParametersAreEqual()
+            {
+                var test1 = new TestClass { IntValue = 99 };
+                var test2 = new TestClass { IntValue = 99 };
+                var subject = new PropertyEqualityComparer<TestClass, int>(x => x.IntValue);
+
+                var results = subject.Equals(test1, test2);
+
+                results.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void ReturnsFalse_IfProjectionsOfBothParametersAreNotEqual()
+            {
+                var test1 = new TestClass { IntValue = 99 };
+                var test2 = new TestClass { IntValue = 42 };
+                var subject = new PropertyEqualityComparer<TestClass, int>(x => x.IntValue);
+
+                var results = subject.Equals(test1, test2);
+
+                results.ShouldBeFalse();
+            }
+
+            [Fact]
+            public void ReturnsTrue_IfProjectionsOfBothParametersAreEqualWithComparer()
+            {
+                var test1 = new TestClass { StrValue = "TESTING" };
+                var test2 = new TestClass { StrValue = "testing" };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue, StringComparer.OrdinalIgnoreCase);
+
+                var results = subject.Equals(test1, test2);
+
+                results.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void ReturnsFalse_IfProjectionsOfBothParametersAreNotEqualWithComparer()
+            {
+                var test1 = new TestClass { StrValue = "TESTING" };
+                var test2 = new TestClass { StrValue = "testing" };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue, StringComparer.Ordinal);
+
+                var results = subject.Equals(test1, test2);
+
+                results.ShouldBeFalse();
+            }
+
+            [Fact]
+            public void IsReflexive_IfCalledWithComparer()
+            {
+                var test = new TestClass { StrValue = "testing" };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue, StringComparer.Ordinal);
+
+                var results = subject.Equals(test, test);
+
+                results.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void IsReflexive_IfCalledWithoutComparer()
+            {
+                var test = new TestClass { StrValue = "testing" };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue);
+
+                var results = subject.Equals(test, test);
+
+                results.ShouldBeTrue();
+            }
+        }
+
+        public class GetHashCodeTests
+        {
+            [Fact]
+            public void ReturnsSameValue_IfProjectionsOfDifferentObjectsAreEqual()
+            {
+                var test1 = new TestClass { StrValue = "testing" };
+                var test2 = new TestClass { StrValue = "testing" };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue);
+
+                var results1 = subject.GetHashCode(test1);
+                var results2 = subject.GetHashCode(test2);
+                var results3 = subject.Equals(test1, test2);
+
+                results3.ShouldBeTrue();
+                results1.ShouldBe(results2);
+            }
+
+            [Fact]
+            public void ReturnsSameValue_IfCalledMultipleSubsequentTimes()
+            {
+                var test = new TestClass { StrValue = "This is a test." };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue);
+
+                var results1 = subject.GetHashCode(test);
+                var results2 = subject.GetHashCode(test);
+
+                results1.ShouldBe(results2);
+            }
+
+            [Fact]
+            public void ReturnsSameValue_IfProjectionsOfDifferentObjectsAreEqualWithComparer()
+            {
+                var test1 = new TestClass { StrValue = "TESTING" };
+                var test2 = new TestClass { StrValue = "testing" };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue, StringComparer.OrdinalIgnoreCase);
+
+                var results1 = subject.GetHashCode(test1);
+                var results2 = subject.GetHashCode(test2);
+                var results3 = subject.Equals(test1, test2);
+
+                results3.ShouldBeTrue();
+                results1.ShouldBe(results2);
+            }
+
+            [Fact]
+            public void ReturnsSameValue_IfCalledMultipleSubsequentTimesWithComparer()
+            {
+                var test = new TestClass { StrValue = "This is a test." };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue, StringComparer.Ordinal);
+
+                var results1 = subject.GetHashCode(test);
+                var results2 = subject.GetHashCode(test);
+
+                results1.ShouldBe(results2);
+            }
+
+            [Fact]
+            public void DoesNotThrowException_IfCalledWithNullObject()
+            {
+                var subject = new PropertyEqualityComparer<TestClass, int>(x => x.IntValue);
+
+                Should.NotThrow(() => { subject.GetHashCode(null); });
+            }
+
+            [Fact]
+            public void DoesNotThrowException_IfCalledWithObjectWhoseProjectionIsNull()
+            {
+                var test = new TestClass { StrValue = null };
+                var subject = new PropertyEqualityComparer<TestClass, string>(x => x.StrValue);
+
+                Should.NotThrow(() => { subject.GetHashCode(test); });
+            }
+        }
+    }
+}
---
+++ ConsoleUtils/Utils/PropertyEqualityComparer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Battousai.Utils
+{
+    public class PropertyEqualityComparer<TBase, TProp> : IEqualityComparer<TBase> where TProp : IEquatable<TProp>
+    {
+        private readonly Func<TBase, TProp> projection;
+        private readonly IEqualityComparer<TProp> comparer;
+
+        public PropertyEqualityComparer(Func<TBase, TProp> projection)
+        {
+            if (projection == null)
+                throw new ArgumentNullException(nameof(projection));
+
+            this.projection = projection;
+            this.comparer = null;
+        }
+
+        public PropertyEqualityComparer(Func<TBase, TProp> projection, IEqualityComparer<TProp> comparer)
+        {
+            if (projection == null)
+                throw new ArgumentNullException(nameof(projection));
+
+            this.projection = projection;
+            this.comparer = comparer;
+        }
+
+        public bool Equals(TBase x, TBase y)
+        {
+            if (x == null && y == null)
+                return true;
+
+            if (x == null || y == null)
+                return false;
+
+            var xValue = projection(x);
+            var yValue = projection(y);
+
+            if (xValue == null && yValue == null)
+                return true;
+
+            if (xValue == null || yValue == null)
+                return false;
+
+            if (comparer == null)
+                return xValue.Equals(yValue);
+            else
+                return comparer.Equals(xValue, yValue);
+        }
+
+        public int GetHashCode(TBase obj)
+        {
+            if (obj == null)
+                return 0;
+
+            var value = projection(obj);
+
+            if (value == null)
+                return 0;
+
+            if (comparer == null)
+                return value.GetHashCode();
+            else
+                return comparer.GetHashCode(value);
+        }
+    }
+}
